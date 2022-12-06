4bb1caa7e419da69c37be214db4cba7af3fd0c6f
alex <alessandro.battini@gmail.com>
2022-12-06 08:32:46 +0100

    my solution for day 6

---
+++ Day6.cs
@@ -0,0 +1,41 @@
+﻿using System.Data;
+using System.Linq;
+
+namespace AdventOfCode2022;
+internal class Day6 : AdventOfCodeDay
+{
+    public override int DayNumber => 6;
+    
+    public override string Calculate_1()
+    {
+        var stream = LoadDayFile().ToArray()[0];
+        var startCom = FindSomething(stream,4);
+        return startCom.ToString();
+    }
+
+    private int FindSomething(string input, int packetLenght)
+    {
+        for (int i = 0; i < input.Length-3; ++i)
+        {
+            Dictionary<char, int> localdict = new Dictionary<char, int>();
+            var substring = input.Substring(i, packetLenght);
+            var allchar = substring.ToCharArray();
+            foreach(var c in allchar)
+            {
+                localdict[c] = c;
+                if (localdict.Keys.Count == packetLenght)
+                    return i+packetLenght;
+            }
+        }
+        return 0;
+    }
+
+    public override string Calculate_2()
+    {
+        var stream = LoadDayFile().ToArray()[0];
+        var startCom = FindSomething(stream, 14);
+        return startCom.ToString();
+    }
+
+
+}
