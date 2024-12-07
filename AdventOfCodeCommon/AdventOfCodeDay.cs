936b2fa6e0393b0e33653a201bf29573387b0a28
ilBat77 <alessandro.battini@gmail.com>
2024-12-07 14:47:10 +0100

    refactor common code

--- AdventOfCodeCommon/AdventOfCodeDay.cs
+++ AdventOfCodeCommon/AdventOfCodeDay.cs
@@ -1,4 +1,5 @@
-﻿namespace AdventOfCodeCommon;
+﻿using System.Diagnostics;
+namespace AdventOfCodeCommon;
 
 public abstract class AdventOfCodeDay
 {
@@ -7,7 +8,26 @@ public abstract class AdventOfCodeDay
     public abstract string Calculate_1();
     public abstract string Calculate_2();
 
-    public string DayResults => $"Results for Day {DayNumber} ({ReferenceUrl}) :\n first result : {Calculate_1()}\n second result : {Calculate_2()}";
+    public string DayResults
+    {
+        get
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            var first = Calculate_1();
+            stopwatch.Stop();
+            var firstime = stopwatch.Elapsed.ToString("mm\\:ss\\.fff");
+
+            stopwatch = Stopwatch.StartNew();
+            var second = Calculate_2();
+            stopwatch.Stop();
+            var secondtime = stopwatch.Elapsed.ToString("mm\\:ss\\.fff");
+
+            return $"Results for Day {DayNumber} ({ReferenceUrl}) :\n" +
+                   $"\tfirst result : {first} Elapsed : {firstime}\n" +
+                   $"\tsecond result : {second} Elapsed : {secondtime}";
+        }
+    }
+
 
     public IEnumerable<string> ReadDayFile()
     {
